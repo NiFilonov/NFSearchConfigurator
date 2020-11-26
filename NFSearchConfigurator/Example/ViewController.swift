864752c003d85664e06cc18f675e31bdab7ad2f8
NikitaFilonov <iamnikitafilonov@gmail.com>
2020-11-26 17:27:30 +0500

    Starter pack

---
+++ NFSearchConfigurator/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Никита Филонов on 26.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NFSearchConfigurator/NFSearchConfigurator/NFSearchConfigurator.h
@@ -0,0 +1,18 @@
+//
+//  NFSearchConfigurator.h
+//  NFSearchConfigurator
+//
+//  Created by Никита Филонов on 26.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NFSearchConfigurator.
+FOUNDATION_EXPORT double NFSearchConfiguratorVersionNumber;
+
+//! Project version string for NFSearchConfigurator.
+FOUNDATION_EXPORT const unsigned char NFSearchConfiguratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NFSearchConfigurator/PublicHeader.h>
+
+
